834a92ddffc3613a1b343de9b7629f17dab4d45f
Adrianah18 <adrianasxavier18@gmail.com>
2024-09-26 21:48:39 -0300

    localStorage

--- src/app/empresas/create/page.js
+++ src/app/empresas/create/page.js
@@ -1,67 +1,80 @@
-'use client';
+'use client'
 
 import Pagina from "@/app/components/Pagina";
 import { Formik } from "formik";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Button, Form } from "react-bootstrap";
 import { FaCheck } from "react-icons/fa";
-import { IoChevronBack } from "react-icons/io5";
+import { MdOutlineArrowBack } from "react-icons/md";
 
-export default function Page(){
-    function salvar(dados){
-        console.log(dados)
-    }
+export default function Page() {
 
+    const route = useRouter
 
-    return(
-        <Pagina titulo ="empresa">
-            <Formik
-            initialValues={{ nome: '', logo: '' }}
-            onSubmit={values=>salvar(values)}
-            >
-            {({
-                values,
-                handleChange,
-                handleSubmit,
-            }) => (
+     function salvar(dados){
+        const empresas = JSON.parse(localStorage.getItem('empresas')) || []
+        empresas.push(dados)
+        localStorage.setItem('empresas', JSON.stringify(empresas))
+        return route.push('/empresas')
+ }
+ 
+    return (
+    <Pagina titulo="Empresa">
+    
+     <Formik
+    initialValues={{nome: '', logo: '', site: ''}}
+    onSubmit={values=>salvar(values)}>
+        {({ 
+        values,
+        handleChange,
+        handleSubmit,
+        }) => (
+   
+   <Form>
+     <Form.Group className="mb-3" controlId="nome">
+     <Form.Label>Nome</Form.Label>
+     <Form.Control 
+       type="text" 
+       name="nome" 
+       value={values.nome}
+       onChange={handleChange('nome')}
+    />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="logo">
+    <Form.Label>Logo</Form.Label>
+    <Form.Control 
+       type="text" 
+       name="logo"
+       value={values.logo}
+       onChange={handleChange('logo')}
+       />
+    </Form.Group>
 
-           
-
-
-        <Form>
-            <Form.Group className="mb-3" controlId="nome">
-            <Form.Label>Nome</Form.Label>
-            <Form.Control 
-                type="nome" 
-                name="nome"
-                value={values.nome}
-                onChange={handleChange('nome')}
-                />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="logo">
-            <Form.Label>Logo</Form.Label>
-            <Form.Control 
-                type="nome" 
-                name="logo"
-                value={values.logo}
-                onChange={handleChange('logo')}
-                />
-            </Form.Group>
-            <div className="text-center">
-                <Button onClick={handleSubmit} variant="success">
-                    <FaCheck></FaCheck>salvar
-                </Button>
-                <Link href="/empresas"className="btn btn-danger ms-2">
-                    <IoChevronBack></IoChevronBack>Voltar</Link>
-
-            </div>
-
-
-        </Form>
-         )}
-         </Formik>
-
-        </Pagina>
-    )
+    <Form.Group className="mb-3" controlId="site">
+    <Form.Label>Site</Form.Label>
+    <Form.Control 
+       type="text" 
+       name="site"
+       value={values.site}
+       onChange={handleChange('site')}
+       />
+    </Form.Group>
+    
+    
+    <div className="text-center">
+    
+        <Button onClick={handleSubmit} variant="success">
+            <FaCheck /> Salvar
+        </Button>
+    <Link href="/empresas" className="btn btn-danger ms-2">
+        <MdOutlineArrowBack /> Voltar
+    </Link>
+    </div>
+    </Form>
+    )}
+    </Formik>
+    </Pagina>
+ )
 }
+
